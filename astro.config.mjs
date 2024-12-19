695c1d7886cacc95a501a5286c69a17e20184cf0
CompileArtisan <praanesh.b.nair@gmail.com>
2024-12-19 14:07:16 +0530

    Restructured it slightly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import mdx from '@astrojs/mdx';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
---
+++ src/content/about.md
@@ -0,0 +1,3 @@
+
+I'll fill this up, later on
+
