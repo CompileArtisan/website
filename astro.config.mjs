bb3ca0734779cdd82dc481e92690fb9e73eb5fab
CompileArtisan <praanesh.b.nair@gmail.com>
2025-01-19 08:02:53 +0000

    Added MathJax in MainLayout.astro to format the LaTeX

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,10 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  integrations: [mdx()], // Add the MDX integration
+  integrations: [mdx()],
   content: {
     collections: {
-      blog: './src/content/blog', // Define the path for the "blog" collection
+      blog: './src/content/blog',
     },
-  },
+  }
 });
