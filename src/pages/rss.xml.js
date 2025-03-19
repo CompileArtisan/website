79af6a5c2a54b36c73f11b5bd5fdb3d31d533e16
CompileArtisan <praanesh.b.nair@gmail.com>
2025-03-19 21:57:59 +0530

    Now the RSS Feed has formatted content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,10 @@
 import { defineCollection, z } from 'astro:content';
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import { marked } from 'marked'; // Import a markdown parser
 
 export async function GET(context) {
   const posts = await getCollection('blog');
-
   return rss({
     title: "Praanesh's Website",
     description: 'Here is my RSS Feed',
@@ -14,7 +14,7 @@ export async function GET(context) {
       pubDate: post.data.date,
       description: post.data.description,
       link: `https://compileartisan.pages.dev/blog/${post.slug}/`,
-      content: post.body,
+      content: marked.parse(post.body),
     })),
     customData: `<language>en-us</language>`,
   });
