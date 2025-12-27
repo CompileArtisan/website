727e524260cfcfe2dc00ecc7ecea0721be3b28bf
CompileArtisan <praanesh.b.nair@gmail.com>
2025-12-28 00:08:34 +0530

    configured for react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://compileartisan.dev',
   adapter: cloudflare(),
-  integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), react()],
   content: {
     collections: {
       blog: "./src/content/blog",
